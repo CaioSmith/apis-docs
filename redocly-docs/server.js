b1e78269cbb49630bdb1184b62a509b5547ff8a9
CaioSmith <smith.caio085@gmail.com>
2024-07-02 10:17:49 -0300

    adicionando servidor js

---
+++ redocly-docs/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const { exec } = require('child_process');
+
+const filePath = path.join(__dirname, 'redocly-docs-manual.yaml');
+const outputDir = path.join(__dirname, 'docs');
+const PORT = process.env.PORT || 3000;
+
+console.log(`Gerando documentação para o arquivo: ${filePath}`);
+
+
+exec(`npx redocly preview-docs ${filePath} --port ${PORT}`, (err, stdout, stderr) => {
+  if (err) {
+    console.error(`Erro ao gerar a documentação: ${stderr}`);
+    process.exit(1);
+  }
+  
+  const server = http.createServer((req, res) => {
+    const indexPath = path.join(outputDir, 'index.html');
+    fs.readFile(indexPath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('404 Not Found');
+        res.end();
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    });
+  });
+
+  server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+});
